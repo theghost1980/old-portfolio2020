015b23c075ad801e7d23eff2d7ecec0fb19a3fad
Saturno Mangieri <saturnob612@gmail.com>
2020-12-30 13:25:41 -0400

    adding favicon and google-plugin-manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,6 +21,18 @@ module.exports = {
         path: `${__dirname}/src/`,
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Saturnoman`,
+        short_name: `Saturnoman`,
+        start_url: `/`,
+        background_color: `#00488E`,
+        theme_color: `#00488E`,
+        display: `browser`,
+        icon: `src/images/favico.png`, // This path is relative to the root of the site.
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-remark`,
